ef64584738af21ef9b257a3c797554c1310ee5a6
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2024-02-08 23:32:25 +0530

    day 8

--- question8/index.js
+++ question8/index.js
@@ -26,3 +26,9 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+// npm i express or npm install express run in your terminal
+//  write these url on your webpage to see the magic 
+
+// http://localhost:3000/positive?number=-33
+// http://localhost:3000/positive?number=3432
